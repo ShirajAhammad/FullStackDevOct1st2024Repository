38acc5afa2c27acb01260ec84490ba48489e728b
ShirajAhammad <sirajahammad135@gmail.com>
2025-01-21 12:24:41 +0530

    Initial Draft

---
+++ ExcelFile/src/ExcelDemo/ReadExcelFile.java
@@ -0,0 +1,20 @@
+package ExcelDemo;
+
+import java.io.FileInputStream;
+
+public class ReadExcelFile {
+    public static void main(String[] args) {
+
+    }
+    private static void readcontent()
+    {
+        FileInputStream fin=null;
+        Workbook wb=null;
+        Sheet sh=null;
+        Cell cell=null;
+        try
+        {
+            fin=new FileInputStream(name:"c//")
+        }
+    }
+}
